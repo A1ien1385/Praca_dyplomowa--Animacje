0ace96245e806d4e1565a3e47e13adb8249292e5
A1ien1385 <damian.zawadzki@tlen.pl>
2022-06-26 01:19:32 +0200

    space invader animation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./scss/main.scss";
 
 function App() {
   return (
-    <div className="menu_box">
-      <h1 className="menu_title">Horror playbox</h1>
-      <div className="menu_elements">
-        <button className="title_btn shinning">Shinning</button>
-        <button className="title_btn psycho">American Psycho</button>
-        <button className="title_btn ring">The Ring</button>
-        <button className="title_btn alien">Alien</button>
-        <button className="title_btn se7en">Se7en</button>
+    <>
+      <div className="space_invader"></div>
+      <div className="menu_box">
+        <h1 className="menu_title">Horror playbox</h1>
+        <div className="menu_elements">
+          <button className="title_btn shinning">Shinning</button>
+          <button className="title_btn psycho">American Psycho</button>
+          <button className="title_btn ring">The Ring</button>
+          <button className="title_btn alien">Alien</button>
+          <button className="title_btn se7en">Se7en</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
