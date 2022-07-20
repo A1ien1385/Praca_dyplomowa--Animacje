5b68808eb915766f7a680a9d40f04f712d540758
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-20 22:48:26 +0200

    Animation start!

--- src/components/Juon.js
+++ src/components/Juon.js
@@ -9,7 +9,18 @@ export default function Juon() {
     const [elements] = wrapper.current.children;
 
     const ToshioHeadBack = elements.getElementById('ACTIVE_x5F_TOSHIOHEADBACK');
-    gsap.set([ToshioHeadBack], {autoAlpha: 1});
+    const ACTIVE_WINDOW_LEFT_WALL = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_LEFT_x5F_WALL');
+    const ACTIVE_WINDOW_1RIGHT_WALL = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_1RIGHT_x5F_WALL');
+    const ACTIVE_WINDOW_RIGHTWALL = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_2_x5F_RIGHTWALL');
+    const ACTIVE_WINDOW_3LEFTHALF = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_3LEFTHALF');
+    const ACTIVE_WINDOW_RIGHTHALF = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_3RIGHTHALF');
+    const ACTIVE_WINDOW_RIGHT_WALL = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_4_x5F_RIGHT_x5F_WALL');
+    const ACTIVE_WINDOW_RIGHT2_WALL = elements.getElementById('ACTIVE_x5F_WINDOW_x5F_5_x5F_RIGHT_x5F_WALL');
+    const ACTIVE_KAYAKOSAEKI_WALKING = elements.getElementById('ACTIVE_x5F_KAYAKOSAEKI_x5F_WALKING');
+    const ACTIVE_KAYAKOFACE = elements.getElementById('ACTIVE_x5F_KAYAKOFACE');
+    const ACTIVE_TOSHIOHEAD_FRONT = elements.getElementById('ACTIVE_x5F_TOSHIOHEAD_x5F_FRONT');
+
+    gsap.set([ToshioHeadBack, ACTIVE_WINDOW_1RIGHT_WALL, ACTIVE_WINDOW_RIGHTWALL, ACTIVE_WINDOW_3LEFTHALF, ACTIVE_WINDOW_RIGHTHALF, ACTIVE_WINDOW_RIGHT_WALL, ACTIVE_WINDOW_RIGHT2_WALL, ACTIVE_KAYAKOSAEKI_WALKING, ACTIVE_KAYAKOFACE, ACTIVE_TOSHIOHEAD_FRONT], {autoAlpha: 0});
   })
 
   return (
