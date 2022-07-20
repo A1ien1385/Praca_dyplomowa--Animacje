b8f6688110ada5c707da1d8eeb1a0afd64bda3ff
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-20 22:32:11 +0200

    Prepared GSAP works!

--- src/components/Juon.js
+++ src/components/Juon.js
@@ -1,9 +1,20 @@
+import React, {useRef, useEffect} from "react";
 import { ReactComponent as JuonScene } from "./svg/Juon.svg";
+import gsap from "gsap";
 
 export default function Juon() {
+  const wrapper = useRef(null);
+
+  useEffect(()=> {
+    const [elements] = wrapper.current.children;
+
+    const ToshioHeadBack = elements.getElementById('ACTIVE_x5F_TOSHIOHEADBACK');
+    gsap.set([ToshioHeadBack], {autoAlpha: 1});
+  })
+
   return (
     <>
-      <div className="animation_box">
+      <div ref={wrapper} className="animation_box">
         <JuonScene/>
       </div>
     </>
