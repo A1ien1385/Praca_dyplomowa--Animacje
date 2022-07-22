36bd6cb0930d64a5ad7e466fa35a5eb5700a8147
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-22 20:14:46 +0200

    Prepare animations environment

--- src/components/TheRing.js
+++ src/components/TheRing.js
@@ -4,9 +4,27 @@ import gsap from "gsap";
 import { TweenMax } from "gsap/gsap-core";
 
 export default function TheRing() {
+  const wrapper = useRef(null);
+
+  useEffect(()=> {
+    const [elements] = wrapper.current.children;
+
+    
+    
+
+    gsap.set([], {autoAlpha: 0, ease: "power3.inout"});
+
+    const tl = gsap.timeline({defaults: {ease: "power3.inout"}});
+
+     
+      
+      
+      
+  })
+
   return (
     <>
-      <div className="animation_box">
+      <div ref={wrapper} className="animation_box">
         <TheRingScene/>
       </div>
     </>
