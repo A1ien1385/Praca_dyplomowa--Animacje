c79dbe4a97c0535b4073d37624eb10522de99264
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-22 20:30:20 +0200

    TheRing set done

--- src/components/TheRing.js
+++ src/components/TheRing.js
@@ -9,10 +9,23 @@ export default function TheRing() {
   useEffect(()=> {
     const [elements] = wrapper.current.children;
 
-    
+    const SadakoFace = elements.getElementById("ACTIVE_x5F_SADAKO_x5F_FACE");
+    const ACTIVE_LIGHT = elements.getElementById("ACTIVE_x5F_TV_x5F_LIGHT");
+    const SADAKOFRAME1 = elements.getElementById("ACTIVE_x5F_SADAKO1");
+    const SADAKOFRAME2 = elements.getElementById("ACTIVE_x5F_SADAKO2");
+    const SADAKOFRAME3 = elements.getElementById("ACTIVE_x5F_SADAKO3");
+    const SADAKOFRAME4 = elements.getElementById("ACTIVE_x5F_SADAKO4");
+    const FOG  = elements.getElementById("ACTIVE_x5F_FOG1");
+    const EYE1 = elements.getElementById("ACTIVE_x5F_EYE1")
+    const EYE2 = elements.getElementById("ACTIVE_x5F_EYE2")
+    const EYE3 = elements.getElementById("ACTIVE_x5F_EYE3")
+    const EYE4 = elements.getElementById("ACTIVE_x5F_EYE4")
+    const EYE5 = elements.getElementById("ACTIVE_x5F_EYE5")
+    const EYE6 = elements.getElementById("ACTIVE_x5F_EYE6")
+    const RING = elements.getElementById('ACTIVE_x5F_RING');
     
 
-    gsap.set([], {autoAlpha: 0, ease: "power3.inout"});
+    gsap.set([SadakoFace, ACTIVE_LIGHT, SADAKOFRAME1, SADAKOFRAME2, SADAKOFRAME3, SADAKOFRAME4, EYE1, EYE2, EYE3, EYE4, EYE5, EYE6, RING], {autoAlpha: 0, ease: "power3.inout"});
 
     const tl = gsap.timeline({defaults: {ease: "power3.inout"}});
 
