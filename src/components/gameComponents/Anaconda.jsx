33ae91f0b55ed00e670b08978bf7ba4a69c37f80
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-29 22:53:21 +0200

    add board

--- src/components/gameComponents/Anaconda.jsx
+++ src/components/gameComponents/Anaconda.jsx
@@ -1,11 +1,25 @@
- const Anaconda = () => {
-    
+import { useState } from "react";
 
-    
-    return (
+ 
+ const BOARDSIZE = 10;
+ 
+ const Anaconda = () => {
+    const [board, setBoard] = useState(
+      new Array(BOARDSIZE).fill(0).map(row => new Array(BOARDSIZE).fill(0)),
+    );
+
+     return (
       <>
-        <div className="animation_box">
-        xdddddddddd
+        <div className="board">
+        {board.map((row, rowIdx) => (
+          <div key={rowIdx} className="row">
+           {
+            row.map((cell, cellIdx) => (
+              <div key={cellIdx} className="cell"></div>
+            ))
+           } 
+          </div>
+        ))}
       </div>
       </>
     );
