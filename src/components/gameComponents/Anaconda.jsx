a011aa34342ca7a641a8147850ba9b32f3ba6240
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-30 18:45:50 +0200

    start snake development

--- src/components/gameComponents/Anaconda.jsx
+++ src/components/gameComponents/Anaconda.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
  
  
  const Anaconda = () => {
+  const [snakeDots, setSnakeDots] = useState([0,0], [2,0]);
     
      return (
       <>
       <div className="animation_box">
-        <div className="game--box"></div>
+        <div className="game--box">
+          <div className="snake-dot" style={{top: 0, left: 0}}></div>
+          <div className="snake-dot" style={{top: 0, left: '2%'}}></div>
+          <div className="snake-dot" style={{top: 0, left: '4%'}}></div>
+        </div>
       </div>
       </>
     );
