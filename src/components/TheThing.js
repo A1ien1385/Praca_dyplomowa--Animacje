2cde8f1881e2cf0b7c08a40e2e7e89cfe07fbc97
A1ien1385 <damian.zawadzki@tlen.pl>
2022-08-01 01:01:35 +0200

    The Thing animation almost done

--- src/components/TheThing.js
+++ src/components/TheThing.js
@@ -9,6 +9,17 @@ export default function TheThing() {
   useEffect(()=> {
     const [elements] = wrapper.current.children;
     
+    const norrisThing = elements.getElementById('ACTIVENORRIS');
+    const palmerThing = elements.getElementById('ACTIVEPALMER');
+    const daySky = elements.getElementById('ACTIVEDAY');
+
+    gsap.set([norrisThing, palmerThing], {ease: "power3.inout"});
+    gsap.set([palmerThing], {y: 400})
+    gsap.set([norrisThing], {x: -600})
+
+    gsap.to(daySky, {delay: 4, duration: 3, autoAlpha: 0});
+    gsap.to(palmerThing, {delay: 6, duration: 1,  y: 0});
+    gsap.to(norrisThing, {delay: 7, duration: 1.5,  x: 0});
         
   })
 
