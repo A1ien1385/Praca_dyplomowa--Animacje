3b7d1d3dc491c5d0bba37d84151a8dc6cc46d6de
A1ien1385 <damian.zawadzki@tlen.pl>
2022-08-02 21:48:36 +0200

    Typing effect working

--- src/components/AboutAuthor.js
+++ src/components/AboutAuthor.js
@@ -1,11 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Typewriter from 'typewriter-effect';
+
 export default function AboutAuthor() {
     
+  const [state] = useState({
+    title: "My name...",
+    titleTwo: "My profession is..",
+    titleThree: "I like..."
+  })
 
-    
-    return (
+      return (
       <>
        <div className="animation_box typing--box">
-        
+           <h2>
+            <div className='typewriter_element'>{state.title}</div>
+            <div className='typewriter_element'>{state.titleTwo}</div>
+            <div className='typewriter_element'>{state.titleThree}</div>
+           </h2>
+
+           <Typewriter
+           options={{
+            autoStart: true,
+            loop: true,
+            delay: 40,
+            strings: [
+              "xdd",
+              "lilili",
+              "barararara",
+            ],
+           }}
+           />
       </div>
       </>
     );
