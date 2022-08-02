ebd0a511fa4918a7a98cd3ebee7f64fcf3f749dc
A1ien1385 <damian.zawadzki@tlen.pl>
2022-08-02 22:44:59 +0200

    About author done

--- src/components/AboutAuthor.js
+++ src/components/AboutAuthor.js
@@ -4,32 +4,35 @@ import Typewriter from 'typewriter-effect';
 export default function AboutAuthor() {
     
   const [state] = useState({
-    title: "My name...",
-    titleTwo: "My profession is..",
-    titleThree: "I like..."
+    linkOne: "GitHub",
+    linkTwo: "Codepen",
+    linkThree: "LinkedIn"
   })
 
       return (
       <>
        <div className="animation_box typing--box">
-           <h2>
-            <div className='typewriter_element'>{state.title}</div>
-            <div className='typewriter_element'>{state.titleTwo}</div>
-            <div className='typewriter_element'>{state.titleThree}</div>
-           </h2>
-
            <Typewriter
            options={{
             autoStart: true,
             loop: true,
-            delay: 40,
+            delay: 20,
             strings: [
-              "xdd",
-              "lilili",
-              "barararara",
+              "Nazywam się Damian...",
+              "Jestem programistą JavaScript i grafikiem komputerowym...",
+              "Wolne chwile poświęcam na oglądanie filmów, czytanie książek, granie w gry PC/PS i na dalszy rozwój w swoim fachu.",
+              "End of the message.",
             ],
            }}
            />
+
+            <ul className='links'>
+            <li> <a className='typing--box links' href='https://github.com/A1ien1385'>{state.linkOne} </a></li>
+            <li><a className='typing--box links' href='https://codepen.io/A1ien_1385'>{state.linkTwo} </a></li>
+            <li><a className='typing--box links' href='https://www.linkedin.com/in/damian-zawadzki-07b2321a5/'>{state.linkThree} </a></li>
+           </ul>
+
+           
       </div>
       </>
     );
